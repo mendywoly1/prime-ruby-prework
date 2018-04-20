07028b7d4641149658b99d229e6e8eb02dd1986c
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-04-20 01:26:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,12 @@
 # Add  code here!
-def prime?(int) 
-  return false if int < 2 
+
+
+
+  return false if num < 2
+
+  (2...num).each do |check_num|
+    return false if num % check_num == 0
+  end
   
-  (2..num).each do |num|
-  return false if int % num == 0
-end
   true
 end
